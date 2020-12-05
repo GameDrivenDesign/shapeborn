50e72187555835ddab1826de3a84446fe9c09ea8
Paul Methfessel <paul.methfessel@web.de>
2020-12-05 14:51:12 +0100

    Add moveable object

--- script/anchor.gd
+++ script/anchor.gd
@@ -1,27 +1,23 @@
-extends Node
-
-
-export(NodePath) var player
+extends Node2D
 
 var highlighted = false
-
 
 func _ready():
 	pass
 
 func on_mouse_entered():
-	$Body/SelectedSprite.visible = true
-	$Body/Sprite.visible = false
+	print("enter")
+	$SelectedSprite.visible = true
+	$Sprite.visible = false
 	highlighted = true
 
 func on_mouse_exited():
-	$Body/SelectedSprite.visible = false
-	$Body/Sprite.visible = true
+	$SelectedSprite.visible = false
+	$Sprite.visible = true
 	highlighted = false
 
 func _process(delta):
 	if Input.is_action_pressed("mouse_left") and highlighted:
-		get_node(player).pull_on(self, delta)
+		$"../Player".pull_on(self, delta)
 	if Input.is_action_pressed("mouse_right") and highlighted:
-		
-		get_node(player).push_on(self, delta)
+		$"../Player".push_on(self, delta)
