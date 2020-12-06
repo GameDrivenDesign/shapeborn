291f31f438e2b2c8b8e7ec56f8c1960af769438c
Paul Methfessel <paul.methfessel@web.de>
2020-12-06 17:52:37 +0100

    Add boss room

--- script/Wall.gd
+++ script/Wall.gd
@@ -6,3 +6,7 @@ export var dark = false setget set_dark
 func set_dark(b):
 	dark = b
 	modulate = Color(0.15, 0.15, 0.15) if dark else Color.white
+
+func set_enabled(b):
+	visible = b
+	$CollisionShape2D.call_deferred("set_disabled", not b)
