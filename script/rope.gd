44aec7b3b0806661a5eb02e366ca7f812995cab0
Paul Methfessel <paul.methfessel@web.de>
2020-12-06 14:36:39 +0100

    Add rope to scene

--- script/rope.gd
+++ script/rope.gd
@@ -1,22 +1,23 @@
-extends RigidBody2D
+extends StaticBody2D
 
 
 var PIECE = preload("res://2d/Piece.tscn")
+var LAST = preload("res://2d/MoveableAnchor.tscn")
 
-export (NodePath) var last
 export (int) var pieces = 1
 
 func _ready():
 	var parent = self
 	for i in range(pieces):
 		parent = add_piece(parent)
-	var joint = parent.get_node("joint")
-	joint.add_child(get_node(last))
+	var joint = parent.get_node("body/joint")
+	var last = LAST.instance()
+	joint.add_child(last)
 	joint.node_a = parent.get_path()
-	joint.node_b = last
+	joint.node_b = last.get_path()
 
 func add_piece(parent):
-	var joint = parent.get_node("joint")
+	var joint = parent.get_node("body/joint")
 	var piece = PIECE.instance()
 	joint.add_child(piece)
 	joint.node_a = parent.get_path()
