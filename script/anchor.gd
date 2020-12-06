d98762b8989afcb7c6230d5ad65383dba96411bc
Paul Methfessel <paul.methfessel@web.de>
2020-12-06 17:15:19 +0100

    Fix selection

--- script/anchor.gd
+++ script/anchor.gd
@@ -8,37 +8,17 @@ func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
 
 func highlight(highlighted):
-	if highlighted:
-		for o in get_tree().get_nodes_in_group("anchor"):
-			if o != self:
-				o.highlight(false)
-	else:
-		# do not deselect immediately; instead wait for the mouse button to be
-		# raised so that the user has more control with many anchors on the screen
-		# selected = false
-		update()
-	
 	self.highlighted = highlighted
 	show_highlight(highlighted)
 
+func select(selected):
+	if not selected:
+		highlight(false)
+	self.selected = selected
+	update()
+
 func _process(delta):
-	if (Input.is_action_just_pressed("mouse_left") 
-		or Input.is_action_just_pressed("mouse_right")) and highlighted:
-		selected = true
-	
-	if (Input.is_action_just_released("mouse_left") 
-		or Input.is_action_just_released("mouse_right")) and selected:
-		selected = false
-		update()
-		if not highlighted:
-			show_highlight(false)
-	
-	if selected and Input.is_action_pressed("mouse_left"):
-		player.pull_on(self, delta)
-		update()
-	
-	if selected and Input.is_action_pressed("mouse_right"):
-		player.push_on(self, delta)
+	if selected:
 		update()
 
 func _draw():
--- script/player.gd
+++ script/player.gd
@@ -7,6 +7,9 @@ export var max_distance = 400
 var start_pos
 var dead = false
 
+var selected_anchor
+var highlighted_anchor
+
 var last_checkpoint
 var time_is_slow = false
 
@@ -16,19 +19,56 @@ func _ready():
 	start_pos = position
 
 func _process(delta):
-	var anchors = get_tree().get_nodes_in_group("anchor")
-	var minAnchor = get_min_anchor(anchors)
-	if minAnchor != null:
-		minAnchor.highlight(true)
-	else:
-		for anchor in anchors:
-			anchor.highlight(false)
+	update_anchors(delta)
 	
 	if Input.is_action_just_pressed("respawn_at_checkpoint"):
 		dead = true
 	
 	if Input.is_action_just_pressed("slow_time") and not time_is_slow :
 		slow_time()
+
+func update_anchors(delta):
+	var anchors = get_anchors()
+	if (Input.is_action_just_pressed("mouse_left") 
+		or Input.is_action_just_pressed("mouse_right")) and highlighted_anchor != null:
+		set_selected_anchor(highlighted_anchor)
+	
+	if (Input.is_action_just_released("mouse_left") 
+		or Input.is_action_just_released("mouse_right")) and selected_anchor != null:
+		set_selected_anchor(null)
+	
+	if selected_anchor != null and Input.is_action_pressed("mouse_left"):
+		pull_on(selected_anchor, delta)
+	
+	if selected_anchor != null and Input.is_action_pressed("mouse_right"):
+		push_on(selected_anchor, delta)
+		
+	# Update highlight
+	if selected_anchor == null:
+		var min_anchor = get_min_anchor(anchors)
+		set_highlighted_anchor(min_anchor)
+	else:
+		# Deselect anchor if too far away. Don't highlight or select other anchor
+		if mouse_dist(selected_anchor) > max_distance:
+			set_selected_anchor(null)
+			
+func set_highlighted_anchor(anchor):
+	var anchors = get_anchors()
+	highlighted_anchor = anchor
+	if anchor != null:
+		highlighted_anchor.highlight(true)
+	for other in anchors:
+		if other != highlighted_anchor:
+			other.highlight(false)
+
+func set_selected_anchor(anchor):
+	var anchors = get_anchors()
+	selected_anchor = anchor
+	if anchor != null:
+		selected_anchor.select(true)
+	for other in anchors:
+		if other != selected_anchor:
+			other.select(false)
 
 const SLOW_FACTOR = 0.3
 func slow_time():
@@ -57,15 +97,20 @@ func get_min_anchor(anchors):
 		return null
 	return minAnchor
 
+func get_anchors():
+	return get_tree().get_nodes_in_group("anchor")
+
 func pull_on(anchor, delta):
 	apply_impulse(Vector2.ZERO, get_impulse(anchor, delta))
 	if anchor is RigidBody2D:
 		anchor.apply_impulse(Vector2.ZERO, -get_impulse(anchor, delta))
+		anchor.update()
 
 func push_on(anchor, delta):
 	apply_impulse(Vector2.ZERO, -get_impulse(anchor, delta))
 	if anchor is RigidBody2D:
 		anchor.apply_impulse(Vector2.ZERO, get_impulse(anchor, delta))
+		anchor.update()
 
 func get_impulse(anchor, delta):
 	return (anchor.global_position - global_position).normalized() * force_strength * delta
